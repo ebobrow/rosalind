d57e02a93091c7eac0b09bb938b534c6423a7ee3
Elliot Bobrow <77182873+ebobrow@users.noreply.github.com>
2022-02-27 17:50:34 -0800

    Create solution.rs

---
+++ revc/solution.rs
@@ -0,0 +1,12 @@
+fn revc(dna: String) -> String {
+    dna.chars()
+        .rev()
+        .map(|c| match c {
+            'A' => 'T',
+            'T' => 'A',
+            'C' => 'G',
+            'G' => 'C',
+            _ => panic!(),
+        })
+        .collect()
+}
