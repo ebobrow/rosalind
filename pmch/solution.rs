6f7167831f31f4a4f6f82cce8ee5859353c285d5
Elliot Bobrow <77182873+ebobrow@users.noreply.github.com>
2022-04-09 19:43:47 -0700

    typo

--- pmch/solution.rs
+++ pmch/solution.rs
@@ -1,13 +1,13 @@
 fn perfect_matchings(dna: String) -> i128 {
     let au = dna.chars().filter(|c| c == &'A').count();
     let gc = dna.chars().filter(|c| c == &'G').count();
-    factorital(au.try_into().unwrap()) * factorital(gc.try_into().unwrap())
+    factorial(au.try_into().unwrap()) * factorital(gc.try_into().unwrap())
 }
 
-fn factorital(n: i128) -> i128 {
+fn factorial(n: i128) -> i128 {
     if n == 0 {
         1
     } else {
-        factorital(n - 1) * n
+        factorial(n - 1) * n
     }
 }
